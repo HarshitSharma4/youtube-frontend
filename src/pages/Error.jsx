2a6415781cadcc914ad09e0a073b8e860eca7d76
harshitSharma4 <harshit.sharma212002@gmail.com>
2024-05-19 18:04:17 +0530

    Feat(Error): create a error 404 page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-xl mb-4">Sorry, the page you are looking for does not exist.</p>
+      <Link to="/" className="text-blue-500 hover:underline">
+        Go back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
